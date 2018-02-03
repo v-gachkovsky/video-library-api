e7a82157b3af17a3d57325ad21afea44da7d1e3e
Vladimir Gachkovsky <v.gachkovsky@gmail.com>
2018-02-04 00:47:47 +0200

    add cors (#5)

--- api.js
+++ api.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 9000;
 const routes = require('./routes');
+
+app.use(cors({
+  origin: 'http://localhost:8080'
+}));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
