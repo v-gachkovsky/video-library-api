e714a622056cfd474c1d19bd67dd1c92816114d0
Vladimir Gachkovsky <v.gachkovsky@gmail.com>
2018-02-03 22:26:46 +0200

    videos urls -> videos ID (#3)

--- database.js
+++ database.js
@@ -2,22 +2,22 @@ const videos = [
   {
     id: 1,
     title: 'Топим каркасную винтовку за 2000 баксов! | Разрушительное ранчо | Перевод Zёбры',
-    url: 'https://www.youtube.com/watch?v=KzY0McjNLE0'
+    url: 'KzY0McjNLE0'
   },
   {
     id: 2,
     title: 'Самые дорогие ружья на крупнейшей оружейной выставке мира | Разрушительное ранчо | Перевод Zёбры',
-    url: 'https://www.youtube.com/watch?v=PXGF4rjhbro'
+    url: 'PXGF4rjhbro'
   },
   {
     id: 3,
     title: 'Оружейная мега-выставка, Трамп-младший и замена Хамви | Разрушительное ранчо | Перевод Zёбры',
-    url: 'https://www.youtube.com/watch?v=QAi_LQUvzFI'
+    url: 'QAi_LQUvzFI'
   },
   {
     id: 4,
     title: 'Правильная стрельба из калаша по-ютубячьи | Разрушительное ранчо | Перевод Zёбры',
-    url: 'https://www.youtube.com/watch?v=-0TaK06iwqc'
+    url: '-0TaK06iwqc'
   }
 ];
 
