b05e32afc19a767c27e6a2a3499a0f1aaec0039b
Vladimir Gachkovsky <v.gachkovsky@gmail.com>
2018-02-12 00:54:07 +0200

    endpoint for fetch courses videos (#8)

--- routes.js
+++ routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const endpoints = require('./endpoints');
-
+// GET
 router.route('/videos').get(endpoints.getVideos);
+router.route('/videos/:courseId').get(endpoints.getCourseVideos);
+// POST
 router.route('/videos').post(endpoints.addVideo);
 
+// GET
 router.route('/courses').get(endpoints.getCourses);
+// POST
 router.route('/courses').post(endpoints.addCourse);
 
 module.exports = router;
