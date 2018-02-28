7de4d0daa6fc7f433203414272d06f6c0ace98db
Vladimir Gachkovsky <v.gachkovsky@gmail.com>
2018-02-28 23:34:13 +0200

    add endpoints for delete entities (#9)

--- routes.js
+++ routes.js
@@ -7,10 +7,15 @@ router.route('/videos').get(endpoints.getVideos);
 router.route('/videos/:courseId').get(endpoints.getCourseVideos);
 // POST
 router.route('/videos').post(endpoints.addVideo);
+// DELETE
+router.route('/videos/:id').delete(endpoints.deleteVideo);
+
 
 // GET
 router.route('/courses').get(endpoints.getCourses);
 // POST
 router.route('/courses').post(endpoints.addCourse);
+// DELETE
+router.route('/courses/:id').delete(endpoints.deleteCourse);
 
 module.exports = router;
